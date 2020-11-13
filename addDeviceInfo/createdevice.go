7b2bc043c2cfdd49f81fc06267faf899009fc989
Amir Mohammad Fallah <amir.78fa@yahoo.com>
2020-11-13 22:53:31 +0330

    [add] Test File

--- addDeviceInfo/createdevice.go
+++ addDeviceInfo/createdevice.go
@@ -3,7 +3,10 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 // Define the standard request body schema
@@ -15,10 +18,6 @@ type deviceInfo struct {
 	Serial      string `json:"serial"`
 }
 
-var (
-	dynaClient dynamodbiface.DynamoDBAPI
-)
-
 func main() {
 	//Init the AWS request handler
 	lambda.Start(handler)
@@ -26,9 +25,32 @@ func main() {
 
 func handler(req deviceInfo) (events.APIGatewayProxyResponse, error) {
 
+	//dyanmodb configs
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	av, err := dynamodbattribute.MarshalMap(req)
+
 	// validate input json
 	if req.ID == "" || req.DeviceModel == "" || req.Name == "" || req.Note == "" || req.Serial == "" {
 		return events.APIGatewayProxyResponse{Body: string("Some values are missing"), StatusCode: 400}, nil
+	}
+
+	// Create item in table Movies
+	tableName := "devices"
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = svc.PutItem(input)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: string("Internal Server Error"), StatusCode: 500}, nil
 	}
 
 	// Send back the response
